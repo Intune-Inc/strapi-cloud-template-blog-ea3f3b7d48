05fb023ee157d9209cc8b870ac9ccdb9367c2234
yun <yun@intune.co.kr>
2025-05-30 17:10:17 +0900

    기본 템플릿 컬렉션 삭제 및 추가 테스트

--- src/api/test/routes/test.js
+++ src/api/test/routes/test.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * category router.
+ * test router
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::category.category');
+module.exports = createCoreRouter('api::test.test');
