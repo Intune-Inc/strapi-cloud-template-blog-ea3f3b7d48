a2b4705f65268504c8ccdf7e8afb134f4289af6a
yun <yun@intune.co.kr>
2025-06-04 15:04:45 +0900

    GolfProduct Collection images 컬럼 타입 변경

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -586,7 +586,10 @@ export interface ApiGolfProductGolfProduct extends Struct.CollectionTypeSchema {
           preset: 'defaultHtml';
         }
       >;
-    images: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    images: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios',
+      true
+    >;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
